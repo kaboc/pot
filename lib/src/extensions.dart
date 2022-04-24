8bba5e2e9c4e3852339fd0077ea50ce2a85cf29f
Kabo <20254485+kaboc@users.noreply.github.com>
2022-04-24 19:48:08 +0900

    Include type name in assert message

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -19,7 +19,7 @@ extension<T> on _PotBody<T> {
         final prevScope = _prevScope;
         if (prevScope != null && Pot._currentScope < prevScope) {
           Pot.$warningPrinter(
-            'A new object was created in an older scope than where the '
+            'A new $T object was created in an older scope than where the '
             'previous object was bound to. It is likely a misuse.\n'
             'If it is not, or if you want to simply suppress this warning, '
             'pass in `suppressWarning: true` to `call()` or `create()`.',
